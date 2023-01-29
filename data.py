090d07a3724ccf26a518b8401a92fc3a55446b8e
barancimen <barancimen321321@gmail.com>
2023-01-30 02:39:11 +0300

    data getter

---
+++ data.py
@@ -0,0 +1,65 @@
+import subprocess
+import pandas as pd
+
+from datetime import timedelta
+from dataclasses import dataclass
+
+@dataclass
+class Tweets:
+    username: str
+    
+    def __post_init__(self):
+        self.load_data()
+        
+    def load_data(self, refresh = False):
+        try:
+            assert not refresh
+            self.data = pd.read_pickle('data')
+        except:
+            self.data = pd.DataFrame()
+            self.save_data()
+    
+    def save_data(self):
+        self.data.to_pickle('data')
+    
+    def query(self, params = {}, seconds = 60):
+        subprocess.call(
+            ' '.join([
+                f'cd twint-zero && timeout {seconds}', 
+                f'go run main.go -Query "from:{self.username}'
+            ] + [
+                f'{k}:{v}' for k,v in params.items()
+            ]) + '" > ../temp', shell = True
+        )
+    
+    def load_temp(self):
+        temp = pd.read_csv('temp', sep = '\t', names = ['id', 'date', 'handle', 'text'])
+        temp = temp[temp['handle'] == f'@{self.username}'].drop('handle', axis = 1)
+        temp['date'] = pd.to_datetime(temp['date'], format = '%b %d, %Y · %I:%M %p %Z')
+
+        return temp.set_index('id')
+                                      
+    def fill(self, forward = True, date = None):        
+        while True:
+            size = self.data.shape[0]
+            try:
+                dates = self.data.date
+                self.query({
+                    'since': date if date else dates.max().strftime('%Y-%m-%d')
+                } if forward else {
+                    'until': date if date else (dates.min() + timedelta(days = 1)).strftime('%Y-%m-%d')
+                })
+            except:
+                self.query()
+            
+            temp = self.load_temp()
+            self.data = pd.concat([self.data, temp]).groupby('id').first() if size > 0 else temp
+            self.save_data()
+            
+            if self.data.shape[0] == size:
+                break
+    
+    def run(self, backward = False):
+        if backward:
+            self.fill(forward = False)
+        self.fill()
---
+++ twint-zero/README.md
@@ -0,0 +1,27 @@
+# Twint Zero
+Like Twint, but zero fat.
+
+# Installation
+1) `git clone https://github.com/twintproject/twint-zero`
+2) `cd twint-zero`
+3) `go mod init twint-zero`
+4) `go mod tidy`
+
+# Usage
+- Without compiling: `go run main.go -Query $QUERY -Instance $INSTANCE`
+- If you compiled... well at this point you are supposed to know.
+
+## CLI Arguments
+1) `$QUERY`: [Here](https://github.com/igorbrigadir/twitter-advanced-search) you go.
+2) `$INSTANCE`: [Here](https://github.com/zedeus/nitter/wiki/Instances) you go.
+
+# Questions/issues
+> Sir, the bill is: five GitHub stars, two forks and one retweet.
+
+That being quoted, feel free to reach out.
+
+# License 
+MIT
+
+# Credits
+[Francesco Poldi](https://twitter.com/noneprivacy)
